2ce68f913ccb70bd31ecf185d89111733b750648
huan <huan.dreamer@gmail.com>
2018-05-10 23:39:16 +0800

    add authentication api

--- securitydemo/src/main/java/top/sillyfan/security/controller/UserController.java
+++ securitydemo/src/main/java/top/sillyfan/security/controller/UserController.java
@@ -1,5 +1,7 @@
 package top.sillyfan.security.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.sillyfan.security.dto.User;
@@ -14,6 +16,11 @@ public class UserController {
     @GetMapping("/user")
     public List<User> query(String name) {
         return Arrays.asList(new User("tom", "123"), new User("Jim", "456"));
+    }
+
+    @GetMapping("/me")
+    public Object getCurrentUser(@AuthenticationPrincipal UserDetails u) {
+        return u;
     }
 
 }
